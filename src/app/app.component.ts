a9b97c4824dc084ccfdbd121944ed54bb29da013
RmNLcN06 <romain.luciano@orange.fr>
2024-11-23 22:34:23 +0100

    Add font & general SCSS styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WordleComponent } from './wordle/wordle.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, WordleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
